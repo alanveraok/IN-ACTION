5a2a32a7172336fe92170df0dd8f0ec0c6dbc943
alan.vera <alanveraok@gmail.com>
2022-08-01 18:54:27 -0300

    Re-entrega

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
+import { Button } from "bootstrap";
+import NavBar from "./components/NavBar";
 
 function App() {
+  const titles = {
+    textAlign: "center",
+    marginTop: "20px",
+    marginBottom: "50px",
+    fontSize: "60px",
+  };
+
   return (
-    <h1>HOPFEN CRAFT BEER</h1>
+    <body>
+      <nav>
+        <NavBar />
+      </nav>
+      <div className="banner">
+        <h1 style={titles}>HOPFEN CRAFT BEER</h1>
+      </div>
+
+      <div class="d-flex justify-content-center">
+        <div class="spinner-border" role="status"></div>
+      </div>
+    </body>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,44 +1,50 @@
 import React from "react";
+import "./NavBar.css"
 
 function NavBar() {
   return (
-    <nav class="navbar navbar-expand-lg bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          HOPFEN CRAFT BEER
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">
-                Inicio
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Nuestras Cervezas
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Venta de insumos
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <body>
+      <header>
+        <nav class="navbar navbar-expand-lg bg-light">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+              HOPFEN CRAFT BEER
+            </a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">
+                    Inicio
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">
+                    Nuestras Cervezas
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">
+                    Venta de insumos
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
+    
+    </body>
   );
 }
 
